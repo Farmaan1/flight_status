7a0fe78928ccfe88dbd5b240529422b7789bd44d
Farmaan <farmaan@flick2know.com>
2024-07-07 13:28:30 +0530

    Added routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FlightTable from './components/FlightTable/FlightTable';
+import FlightDetail from './components/FlightDetail/FlightDetail';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={FlightTable} />
+          <Route path="/flight/:id" component={FlightDetail} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
