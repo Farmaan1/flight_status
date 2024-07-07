a6ee0333c3fa97a34a95405d12b126fcb7a4673a
Farmaan <farmaan@flick2know.com>
2024-07-07 13:33:39 +0530

    Updated React Router to v6 with Routes and Route components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FlightTable from './components/FlightTable/FlightTable';
 import FlightDetail from './components/FlightDetail/FlightDetail';
 
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={FlightTable} />
-          <Route path="/flight/:id" component={FlightDetail} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<FlightTable />} />
+          <Route path="/flight/:id" element={<FlightDetail />} />
+        </Routes>
       </div>
     </Router>
   );
